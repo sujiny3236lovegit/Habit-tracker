9f11bd39f1ead2e2a05cfb446988ce94ce7d9d48
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2020-12-02 19:06:45 +0900

    state + props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './app.css';
-import Habit from './components/habit';
+import Habits from './components/habits';
 
-function App(){
-  return <Habit /> ;
+function App() {
+  return <Habits />;
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
-  );
-
-
+);
